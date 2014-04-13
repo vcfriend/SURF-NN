b033f09c1f56851379ab8e1ff94205603f3a6094
Meng <whenov@gmail.com>
2014-04-13 17:28:32 +0800

    fill training part

--- Train/Train.cpp
+++ Train/Train.cpp
@@ -12,20 +12,54 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    string pos_samples_file(argv[1]);
-    string neg_samples_file(argv[2]);
-
+    /* Read training samples */
+    cout << "Reading training samples..." << endl;
+    string samples_file;
     FileStorage fs;
-    Mat pos_samples;
-    Mat neg_samples;
+    Mat samples;
+    Mat inputs;
+    Mat pos_labels;
+    Mat neg_labels;
+    Mat labels;
 
-    fs.open(pos_samples_file, FileStorage::READ);
-    fs[pos_samples_file.substr(0, pos_samples_file.rfind('.'))] >> pos_samples;
-    fs.open(neg_samples_file, FileStorage::READ);
-    fs[neg_samples_file.substr(0, neg_samples_file.rfind('.'))] >> neg_samples;
+    samples_file = argv[1]; // read positive samples
+    fs.open(samples_file, FileStorage::READ);
+    fs[samples_file.substr(0, samples_file.rfind('.'))] >> samples;
+    inputs.push_back(samples);
+    hconcat(Mat::ones(samples.rows, 1, CV_32F), Mat::zeros(samples.rows, 1, CV_32F), pos_labels);
+
+    samples_file = argv[2]; // read negative samples
+    fs.open(samples_file, FileStorage::READ);
+    fs[samples_file.substr(0, samples_file.rfind('.'))] >> samples;
+    inputs.push_back(samples);
+    hconcat(Mat::ones(samples.rows, 1, CV_32F), Mat::zeros(samples.rows, 1, CV_32F), neg_labels);
+
+    vconcat(pos_labels, neg_labels, labels);
+
+    samples.release();
+    pos_labels.release();
+    neg_labels.release();
     fs.release();
 
+    /* Neural network */
+    cout << "Constructing neural network..." << endl;
+    CvANN_MLP_TrainParams params;
+    params.term_crit = cvTermCriteria(CV_TERMCRIT_ITER + CV_TERMCRIT_EPS, 1000, 0.01);
+    params.train_method = CvANN_MLP_TrainParams::BACKPROP;
+    params.bp_dw_scale = 0.1;
+    params.bp_moment_scale = 0.1;
 
+    Mat layerSizes = (Mat_<int>(1, 3) << 50, 40, 2);
+    CvANN_MLP nn(layerSizes, CvANN_MLP::SIGMOID_SYM, 1, 1); // activation function: alpha=1, beta=1
+
+    cout << "Training neural network..." << endl;
+    nn.train(inputs, labels, Mat(), Mat(), params);
+
+    string model_file = "nn.yml";
+    cout << "Save neural network to " << model_file << "..." << endl;
+    nn.save("nn.yml");
+
+    cout << "Done." << endl;
 
     return 0;
 }
