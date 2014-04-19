520b17479c50aac841bb6b0bf1eba66638a50ce0
Meng <whenov@gmail.com>
2014-04-19 22:53:51 +0800

    fit to output layer size of mlp

--- Evaluate/Evaluate.cpp
+++ Evaluate/Evaluate.cpp
@@ -12,50 +12,42 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    cout << "Reading trained neural network..." << endl;
-    CvANN_MLP nn;
-    nn.load("nn.yml");
-
     /* Read training samples */
     cout << "Reading training samples..." << endl;
     string samples_file;
     FileStorage fs;
     Mat samples;
     Mat inputs;
-    Mat pos_labels;
-    Mat neg_labels;
     Mat labels;
 
     samples_file = argv[1]; // read positive samples
     fs.open(samples_file, FileStorage::READ);
     fs[samples_file.substr(0, samples_file.rfind('.'))] >> samples;
     inputs.push_back(samples);
-    hconcat(Mat::ones(samples.rows, 1, CV_32F), Mat::zeros(samples.rows, 1, CV_32F), pos_labels);
+    labels.push_back(Mat(Mat::ones(samples.rows, 1, CV_32F)));
 
     samples_file = argv[2]; // read negative samples
     fs.open(samples_file, FileStorage::READ);
     fs[samples_file.substr(0, samples_file.rfind('.'))] >> samples;
     inputs.push_back(samples);
-    hconcat(Mat::zeros(samples.rows, 1, CV_32F), Mat::ones(samples.rows, 1, CV_32F), neg_labels);
-
-    vconcat(pos_labels, neg_labels, labels);
+    labels.push_back(Mat(Mat::zeros(samples.rows, 1, CV_32F)));
 
     samples.release();
-    pos_labels.release();
-    neg_labels.release();
     fs.release();
+
+    /* Neural Network */
+    cout << "Reading trained neural network..." << endl;
+    CvANN_MLP nn;
+    nn.load("nn.yml");
 
     Mat resp;
     nn.predict(inputs, resp);
 
-    fs.open("resp.yml", FileStorage::WRITE);
-    fs << "resp" << resp;
-
-    Mat binary_resp;
-    threshold(resp, binary_resp, 0, 1, THRESH_BINARY);
+    Mat preds;
+    threshold(resp, preds, 0, 1, THRESH_BINARY);
 
     Mat diff;
-    compare(labels.col(1), binary_resp.col(1), diff, CMP_NE);
+    compare(labels, preds, diff, CMP_NE);
     int nz = countNonZero(diff);
 
     return 0;
